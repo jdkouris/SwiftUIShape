91f36bdf41b3b155936900d11dd803127f9d979c
John Kouris <jdkouris@gmail.com>
2021-05-01 07:27:10 -0700

    Initial Commit

---
+++ SwiftUIShape/SwiftUIShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShapeApp.swift
+//  SwiftUIShape
+//
+//  Created by John Kouris on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
