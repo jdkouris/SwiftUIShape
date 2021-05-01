c59df4c34eaffd2f9211df684ef7070cd139ffe4
John Kouris <jdkouris@gmail.com>
2021-05-01 07:33:22 -0700

    Added a shape with a border

--- SwiftUIShape/ContentView.swift
+++ SwiftUIShape/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Path() { path in
+                path.move(to: CGPoint(x: 20, y: 60))
+                path.addLine(to: CGPoint(x: 40, y: 60))
+                path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 0))
+                path.addLine(to: CGPoint(x: 230, y: 60))
+                path.addLine(to: CGPoint(x: 230, y: 100))
+                path.addLine(to: CGPoint(x: 20, y: 100))
+            }
+            .fill(Color.purple)
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 20, y: 60))
+                path.addLine(to: CGPoint(x: 40, y: 60))
+                path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 0))
+                path.addLine(to: CGPoint(x: 230, y: 60))
+                path.addLine(to: CGPoint(x: 230, y: 100))
+                path.addLine(to: CGPoint(x: 20, y: 100))
+                path.closeSubpath()
+            }
+            .stroke(Color.black, lineWidth: 5)
+        }
     }
 }
 
