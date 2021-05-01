7db26785bf5a5ce3baeba1a9e9f9725dd3103cde
John Kouris <jdkouris@gmail.com>
2021-05-01 07:51:37 -0700

    Added a rectangle with dome shape

--- SwiftUIShape/ContentView.swift
+++ SwiftUIShape/ContentView.swift
@@ -9,46 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Path { path in
-                path.move(to: CGPoint(x: 187, y: 187))
-                path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: 190), clockwise: true)
+        VStack(spacing: 30) {
+            ZStack {
+                Path { path in
+                    path.move(to: CGPoint(x: 187, y: 187))
+                    path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: 190), clockwise: true)
+                }
+                .fill(Color(.systemYellow))
+                
+                Path { path in
+                    path.move(to: CGPoint(x: 187, y: 187))
+                    path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 190.0), endAngle: Angle(degrees: 110), clockwise: true)
+                }
+                .fill(Color(.systemTeal))
+                
+                Path { path in
+                    path.move(to: CGPoint(x: 187, y: 187))
+                    path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 110.0), endAngle: Angle(degrees: 90), clockwise: true)
+                }
+                .fill(Color(.systemBlue))
+                
+                Path { path in
+                    path.move(to: CGPoint(x: 187, y: 187))
+                    path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 90.0), endAngle: Angle(degrees: 360), clockwise: true)
+                }
+                .fill(Color(.systemPurple))
+                .offset(x: 20, y: 20)
+                
+                Path { path in
+                    path.move(to: CGPoint(x: 187, y: 187))
+                    path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 90.0), endAngle: Angle(degrees: 360), clockwise: true)
+                    path.closeSubpath()
+                }
+                .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 10)
+                .offset(x: 20, y: 20)
+                .overlay(
+                    Text("25%")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                        .offset(x: 80, y: 80)
+                )
             }
-            .fill(Color(.systemYellow))
             
             Path { path in
-                path.move(to: CGPoint(x: 187, y: 187))
-                path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 190.0), endAngle: Angle(degrees: 110), clockwise: true)
+                path.move(to: CGPoint(x: 0, y: 0))
+                path.addQuadCurve(to: CGPoint(x: 200, y: 0), control: CGPoint(x: 100, y: -20))
+                path.addLine(to: CGPoint(x: 200, y: 40))
+                path.addLine(to: CGPoint(x: 200, y: 40))
+                path.addLine(to: CGPoint(x: 0, y: 40))
             }
-            .fill(Color(.systemTeal))
-            
-            Path { path in
-                path.move(to: CGPoint(x: 187, y: 187))
-                path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 110.0), endAngle: Angle(degrees: 90), clockwise: true)
-            }
-            .fill(Color(.systemBlue))
-            
-            Path { path in
-                path.move(to: CGPoint(x: 187, y: 187))
-                path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 90.0), endAngle: Angle(degrees: 360), clockwise: true)
-            }
-            .fill(Color(.systemPurple))
-            .offset(x: 20, y: 20)
-            
-            Path { path in
-                path.move(to: CGPoint(x: 187, y: 187))
-                path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 90.0), endAngle: Angle(degrees: 360), clockwise: true)
-                path.closeSubpath()
-            }
-            .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 10)
-            .offset(x: 20, y: 20)
-            .overlay(
-                Text("25%")
-                    .font(.system(.largeTitle, design: .rounded))
-                    .bold()
-                    .foregroundColor(.white)
-                    .offset(x: 80, y: -100)
-            )
+            .fill(Color.green)
         }
     }
 }
