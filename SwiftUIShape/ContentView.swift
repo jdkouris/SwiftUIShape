513346992a1aa451569d9656cea6e59eee92116b
John Kouris <jdkouris@gmail.com>
2021-05-01 07:45:40 -0700

    Added a border and text overlay

--- SwiftUIShape/ContentView.swift
+++ SwiftUIShape/ContentView.swift
@@ -31,9 +31,24 @@ struct ContentView: View {
             Path { path in
                 path.move(to: CGPoint(x: 187, y: 187))
                 path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 90.0), endAngle: Angle(degrees: 360), clockwise: true)
-                
             }
             .fill(Color(.systemPurple))
+            .offset(x: 20, y: 20)
+            
+            Path { path in
+                path.move(to: CGPoint(x: 187, y: 187))
+                path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 90.0), endAngle: Angle(degrees: 360), clockwise: true)
+                path.closeSubpath()
+            }
+            .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 10)
+            .offset(x: 20, y: 20)
+            .overlay(
+                Text("25%")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                    .offset(x: 80, y: -100)
+            )
         }
     }
 }
