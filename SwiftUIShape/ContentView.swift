8200e160289e6a8cc29a5c21fc1a3668e66ac04e
John Kouris <jdkouris@gmail.com>
2021-05-01 08:19:43 -0700

    Added the progress indicator

--- SwiftUIShape/ContentView.swift
+++ SwiftUIShape/ContentView.swift
@@ -8,58 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var purpleGradient = LinearGradient(gradient:
+                                                    Gradient(colors:
+                                                                [Color(red: 207/255, green: 150/255, blue: 207/255),
+                                                                 Color(red: 107/255, green: 116/255, blue: 179/255)]
+                                                    ),
+                                                startPoint: .trailing,
+                                                endPoint: .leading)
+    
     var body: some View {
         VStack(spacing: 30) {
+            
+            ZStack {
+                Circle()
+                    .stroke(Color(.systemGray6), lineWidth: 20)
+                    .frame(width: 300, height: 300)
+                Circle()
+                    .trim(from: 0, to: 0.85)
+                    .stroke(purpleGradient, lineWidth: 20)
+                    .frame(width: 300, height: 300)
+                    .overlay(
+                        VStack {
+                            Text("85%")
+                                .font(.system(size: 80, weight: .bold, design: .rounded))
+                                .foregroundColor(Color(.systemGray))
+                            Text("Complete")
+                                .font(.system(.body, design: .rounded))
+                                .bold()
+                                .foregroundColor(Color(.systemGray))
+                        }
+                    )
+            }
+            
             ZStack {
                 Path { path in
                     path.move(to: CGPoint(x: 187, y: 187))
-                    path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: 190), clockwise: true)
+                    path.addArc(center: .init(x: 187, y: 187), radius: 100, startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: 190), clockwise: true)
                 }
                 .fill(Color(.systemYellow))
                 
                 Path { path in
                     path.move(to: CGPoint(x: 187, y: 187))
-                    path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 190.0), endAngle: Angle(degrees: 110), clockwise: true)
+                    path.addArc(center: .init(x: 187, y: 187), radius: 100, startAngle: Angle(degrees: 190.0), endAngle: Angle(degrees: 110), clockwise: true)
                 }
                 .fill(Color(.systemTeal))
                 
                 Path { path in
                     path.move(to: CGPoint(x: 187, y: 187))
-                    path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 110.0), endAngle: Angle(degrees: 90), clockwise: true)
+                    path.addArc(center: .init(x: 187, y: 187), radius: 100, startAngle: Angle(degrees: 110.0), endAngle: Angle(degrees: 90), clockwise: true)
                 }
                 .fill(Color(.systemBlue))
                 
                 Path { path in
                     path.move(to: CGPoint(x: 187, y: 187))
-                    path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 90.0), endAngle: Angle(degrees: 360), clockwise: true)
+                    path.addArc(center: .init(x: 187, y: 187), radius: 100, startAngle: Angle(degrees: 90.0), endAngle: Angle(degrees: 360), clockwise: true)
                 }
                 .fill(Color(.systemPurple))
                 .offset(x: 20, y: 20)
                 
                 Path { path in
                     path.move(to: CGPoint(x: 187, y: 187))
-                    path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: Angle(degrees: 90.0), endAngle: Angle(degrees: 360), clockwise: true)
+                    path.addArc(center: .init(x: 187, y: 187), radius: 100, startAngle: Angle(degrees: 90.0), endAngle: Angle(degrees: 360), clockwise: true)
                     path.closeSubpath()
                 }
-                .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 10)
+                .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 5)
                 .offset(x: 20, y: 20)
                 .overlay(
                     Text("25%")
-                        .font(.system(.largeTitle, design: .rounded))
+                        .font(.system(.title, design: .rounded))
                         .bold()
                         .foregroundColor(.white)
-                        .offset(x: 80, y: 50)
+                        .offset(x: 60, y: 80)
                 )
             }
-            
-            Circle()
-                .foregroundColor(.green)
-                .frame(width: 200, height: 200)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 5)
-                        .frame(width: 80, height: 80)
-                        .foregroundColor(.white)
-                )
             
             Button(action: {
                 
